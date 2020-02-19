161e21753c1f02888e255f4872bd45ab023d8ac5
Mike Koval <mike@magiccube.co>
2020-02-18 16:54:38 -0800

    tmp
    Signed-off-by: Mike Koval <mike@magiccube.co>

--- android/app/src/main/java/pokedex/mike/pokedex/MovesTable.java
+++ android/app/src/main/java/pokedex/mike/pokedex/MovesTable.java
@@ -1,17 +1,30 @@
 package pokedex.mike.pokedex;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class MovesTable extends TableLayout {
     private String directory;
     private ArrayList<TableButton> buttons;
+    GradientDrawable RECTANGLE;
+
+    View abilitiesView;
+    View tmsView;
+    View hmsView;
 
     public MovesTable(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -25,8 +38,13 @@ public class MovesTable extends TableLayout {
     }
 
     private void init() {
+        RECTANGLE = (GradientDrawable)getResources().getDrawable( R.drawable.rectangle_thin_background );
+        RECTANGLE.setColor(Color.LTGRAY);
+
+
         TableRow tabRow = new TableRow(getContext());
         ArrayList<String> buttonLabels = new ArrayList<>();
+
         buttons = new ArrayList<>();
 
         buttonLabels.add("Abilities");
@@ -42,8 +60,131 @@ public class MovesTable extends TableLayout {
             tabRow.addView(buttons.get(i));
         }
 
-        buttons.get(0).select();
         addView(tabRow);
+
+        abilitiesView = createAbilities();
+        tmsView = createTMS();
+        hmsView = createHMS();
+        addView(abilitiesView);
+        addView(tmsView);
+        addView(hmsView);
+        buttons.get(0).setView(abilitiesView);
+        buttons.get(1).setView(tmsView);
+        buttons.get(2).setView(hmsView);
+
+        buttons.get(0).select();
+
+    }
+
+    public TableRow createTableTitle(String c1, String c2, String c3, int color, int textColor) {
+        TableRow row = new TableRow(getContext());
+        row.setWeightSum(1.1f);
+        {
+            TextView level = new TextView(getContext());
+            level.setText(c1);
+            level.setTextColor(textColor);
+            level.setBackgroundColor(color);
+
+            level.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+            level.setPadding(0,15, 0,15);
+
+            TableRow.LayoutParams span = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            span.weight = 0.2f;
+            span.bottomMargin = 1;
+            span.topMargin = 1;
+
+            level.setLayoutParams(span);
+
+            row.addView(level);
+        }
+
+        {
+            TextView ability = new TextView(getContext());
+            ability.setText(c2);
+            ability.setTextColor(textColor);
+            ability.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+            ability.setBackgroundColor(color);
+
+            ability.setPadding(0,15, 0,15);
+
+            TableRow.LayoutParams span = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            span.weight = 0.6f;
+            span.bottomMargin = 1;
+            span.leftMargin = 1;
+            span.rightMargin = 1;
+            span.topMargin = 1;
+
+            ability.setLayoutParams(span);
+
+            row.addView(ability);
+        }
+
+        {
+            TextView type = new TextView(getContext());
+            type.setText(c3);
+            type.setTextColor(textColor);
+            type.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+            type.setBackgroundColor(color);
+
+            type.setPadding(0,15, 0,15);
+
+
+            TableRow.LayoutParams span = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            span.weight = 0.3f;
+            span.bottomMargin = 1;
+            span.topMargin = 1;
+
+
+            type.setLayoutParams(span);
+
+            row.addView(type);
+        }
+        return row;
+    }
+
+    public View createAbilities() {
+
+        TableLayout view = new TableLayout(getContext());
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(params);
+        view.addView(createTableTitle("Level", "Move", "Type", TITLE_COLOR, TITLE_TEXT_COLOR));
+
+        for(int i =0 ; i < 10; i++) {
+            view.addView(createTableTitle("" + i, "FOO", "ELECTRIC", ROW_COLOR, ROW_TEXT_COLOR));
+        }
+        return view;
+
+    }
+
+    public View createTMS() {
+
+        TableLayout view = new TableLayout(getContext());
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(params);
+        view.addView(createTableTitle("TM", "Move", "Type", TITLE_COLOR, TITLE_TEXT_COLOR));
+
+        for(int i =0 ; i < 10; i++) {
+            view.addView(createTableTitle("" + i, "TM", "FIRE", ROW_COLOR, ROW_TEXT_COLOR));
+        }
+        return view;
+
+    }
+
+    public View createHMS() {
+
+        TableLayout view = new TableLayout(getContext());
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(params);
+        view.addView(createTableTitle("HM", "Move", "Type", TITLE_COLOR, TITLE_TEXT_COLOR));
+
+        for(int i =0 ; i < 5; i++) {
+            view.addView(createTableTitle("" + i, "HM", "WATER", ROW_COLOR, ROW_TEXT_COLOR));
+        }
+        return view;
+
     }
 
     public void setDirectory(String directory){
--- android/app/src/main/java/pokedex/mike/pokedex/TableButton.java
+++ android/app/src/main/java/pokedex/mike/pokedex/TableButton.java
@@ -1,11 +1,14 @@
 package pokedex.mike.pokedex;
 
 import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import java.util.ArrayList;
 
@@ -15,6 +18,7 @@ public class TableButton extends Button {
     private int SELECTED_COLOR = Color.GRAY;
     private int BASE_COLOR = Color.LTGRAY;
     private ArrayList<TableButton> buttons;
+    private View view;
 
     public TableButton(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -47,14 +51,25 @@ public class TableButton extends Button {
 
     public void select() {
         setBackgroundColor(SELECTED_COLOR);
+        if(view != null) {
+            view.setVisibility(VISIBLE);
+        }
     }
 
     public void deselect() {
         setBackgroundColor(BASE_COLOR);
+        if(view != null){
+            view.setVisibility(GONE);
+
+        }
     }
 
     public void setButtons(ArrayList<TableButton> list) {
         buttons = list;
+    }
+
+    public void setView (View view) {
+        this.view = view;
     }
 
 }
